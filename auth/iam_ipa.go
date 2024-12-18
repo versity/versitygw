8101b35befe0978d8dd683d9cff5ab91184d47cc
Yuriy Halytskyy <yuriy.halytskyy@gmail.com>
2024-12-19 09:53:11 +1300

    get secret from IPA vault

--- auth/iam_ipa.go
+++ auth/iam_ipa.go
@@ -1,18 +1,25 @@
 package auth
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/rsa"
 	"crypto/tls"
+	"crypto/x509"
 	b64 "encoding/base64"
 	"encoding/json"
+	"encoding/pem"
+	"errors"
 	"fmt"
-	"github.com/mitchellh/mapstructure"
 	"io"
-	"math/rand"
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
 	"strconv"
 	"strings"
+
+	"github.com/mitchellh/mapstructure"
 )
 
 type IpaIAMService struct {
@@ -81,6 +88,15 @@ type IpaUser struct {
 	Uidnumber []string
 }
 
+type IpaVaultData struct {
+	Nonce struct {
+		Base64 string `mapstructure:"__base64__"`
+	} `mapstructure:"nonce"`
+	Vault_data struct {
+		Base64 string `mapstructure:"__base64__"`
+	} `mapstructure:"vault_data"`
+}
+
 func (ipa *IpaIAMService) rpc(input string) (IpaResult, error) {
 	req, err := http.NewRequest("POST", fmt.Sprintf("https://%s/ipa/session/json", ipa.host), strings.NewReader(input))
 	if err != nil {
@@ -100,7 +116,8 @@ func (ipa *IpaIAMService) rpc(input string) (IpaResult, error) {
 	}
 
 	data := IpaResult{}
-	fmt.Println(json.Unmarshal(bytes, &data))
+	json.Unmarshal(bytes, &data)
+	fmt.Println(data)
 
 	return data, nil
 
@@ -121,6 +138,44 @@ func NewIpaIAMService(rootAcc Account, host, username, password string) (IAMServ
 
 func (ipa *IpaIAMService) CreateAccount(account Account) error {
 	return fmt.Errorf("not implemented")
+}
+
+// PKCS7 errors.
+var (
+	// ErrInvalidBlockSize indicates hash blocksize <= 0.
+	ErrInvalidBlockSize = errors.New("invalid blocksize")
+
+	// ErrInvalidPKCS7Data indicates bad input to PKCS7 pad or unpad.
+	ErrInvalidPKCS7Data = errors.New("invalid PKCS7 data (empty or not padded)")
+
+	// ErrInvalidPKCS7Padding indicates PKCS7 unpad fails to bad input.
+	ErrInvalidPKCS7Padding = errors.New("invalid padding on input")
+)
+
+// pkcs7Unpad validates and unpads data from the given bytes slice.
+// The returned value will be 1 to n bytes smaller depending on the
+// amount of padding, where n is the block size.
+func pkcs7Unpad(b []byte, blocksize int) ([]byte, error) {
+	if blocksize <= 0 {
+		return nil, ErrInvalidBlockSize
+	}
+	if b == nil || len(b) == 0 {
+		return nil, ErrInvalidPKCS7Data
+	}
+	if len(b)%blocksize != 0 {
+		return nil, ErrInvalidPKCS7Padding
+	}
+	c := b[len(b)-1]
+	n := int(c)
+	if n == 0 || n > len(b) {
+		return nil, ErrInvalidPKCS7Padding
+	}
+	for i := 0; i < n; i++ {
+		if b[len(b)-n+i] != c {
+			return nil, ErrInvalidPKCS7Padding
+		}
+	}
+	return b[:len(b)-n], nil
 }
 
 func (ipa *IpaIAMService) GetUserAccount(access string) (Account, error) {
@@ -156,9 +211,26 @@ func (ipa *IpaIAMService) GetUserAccount(access string) (Account, error) {
 		return Account{}, err
 	}
 
-	b := make([]byte, 256)
+	b := make([]byte, 16)
 	rand.Read(b)
-	base64Key := b64.StdEncoding.EncodeToString(b)
+
+	ipa_cert := `
+-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAv8l+tmcQ+hvZXTqWz5DX
+2n6m+CJImAocIbPeqJdYrFrNj6IE+T8xswLU7CwLSdagfitO56l1/fTqJ4cmo2NR
+Yws/PgnOD9EbH/uepfKYXM9E4ictLtyHvfTwuP0L7rwAn5IsSNS0+oTkWk4zO2Ft
+sVUzXUEJG+6Cn/ShdLRi/8BSRoHZQ/rQTjxYWZnKJi+qjLv2JoEqIRjtu1XkwBXq
+Tp28UzFmTNs3IWDGtE+0ewjc7Sey288NUWNYTsuvJre6LoCl6LeIClS52+XfmNH/
+m+I7wTGMqtbBhYr4uaOKBj65/7mhmsqo8o1FW97xnVYji7qJu70JMNubzgnmqjYh
+pQIDAQAB
+-----END PUBLIC KEY-----
+	`
+	block, _ := pem.Decode([]byte(ipa_cert))
+	key, err := x509.ParsePKIXPublicKey(block.Bytes)
+	pubKey, _ := key.(*rsa.PublicKey)
+	println(pubKey)
+	result, _ := rsa.EncryptPKCS1v15(rand.Reader, pubKey, b)
+	println("encoded result " + b64.StdEncoding.EncodeToString(result))
 
 	secret_request_template := `
 	{
@@ -180,17 +252,51 @@ func (ipa *IpaIAMService) GetUserAccount(access string) (Account, error) {
 	}
 	`
 
-	secret_request := fmt.Sprintf(secret_request_template, access, base64Key)
+	secret_request := fmt.Sprintf(secret_request_template, access, b64.StdEncoding.EncodeToString(result))
 	out, _ = ipa.rpc(secret_request)
 	fmt.Println("printing vault data")
-	fmt.Println(out)
+
+	d := IpaVaultData{}
+	mapstructure.Decode(out.Result.Json, &d)
+	fmt.Println(out.Result.Json)
+	fmt.Println(d)
+	aes, err := aes.NewCipher(b)
+
+	fmt.Println(err)
+	//cbc := aes.CBC{Aes: *eas1, Padding: &PCKS7Padding{}}
+	nonce, _ := b64.StdEncoding.DecodeString(d.Nonce.Base64)
+	cbc := cipher.NewCBCDecrypter(aes, nonce)
+	println(d.Nonce.Base64)
+	println(len(nonce))
+	ctext, _ := b64.StdEncoding.DecodeString(d.Vault_data.Base64)
+	var ptext []byte = make([]byte, len(ctext))
+	cbc.CryptBlocks(ptext, ctext)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(ptext)
+
+	type VaultData struct {
+		Data string `json:"data"`
+	}
+
+	vd := VaultData{}
+
+	ptext_unpadded, err := pkcs7Unpad(ptext, 16)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(json.Unmarshal(ptext_unpadded, &vd))
+	fmt.Println("data part is " + vd.Data)
+
+	secret, _ := b64.StdEncoding.DecodeString(vd.Data)
 
 	uidnumber, _ := strconv.ParseInt(u.Uidnumber[0], 10, 32)
 	gidnumber, _ := strconv.ParseInt(u.Gidnumber[0], 10, 32)
 
 	acc := Account{
 		Access:  access,
-		Secret:  "verysecret",
+		Secret:  string(secret),
 		Role:    RoleUser,
 		UserID:  int(uidnumber),
 		GroupID: int(gidnumber),
