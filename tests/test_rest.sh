b76d2f6b436e4a501b163fba5d9651038b71e3b1
Luke McCrone <lrm110484@gmail.com>
2024-12-21 13:59:46 -0300

    test: more updates

--- tests/test_rest.sh
+++ tests/test_rest.sh
@@ -441,7 +441,7 @@ export RUN_USERS=true
 
 @test "REST - list objects v2 - invalid continuation token" {
   if [ "$DIRECT" != "true" ]; then
-    skip "https://github.com/versity/versitygw/issues/993"
+    skip "https://github.com/versity/versitygw/issues/995"
   fi
   run setup_bucket "s3api" "$BUCKET_ONE_NAME"
   assert_success
@@ -461,11 +461,30 @@ export RUN_USERS=true
   run put_object "s3api" "$TEST_FILE_FOLDER/$test_file_three" "$BUCKET_ONE_NAME" "$test_file_three"
   assert_success
 
-  run list_objects_check_params_get_token "$BUCKET_ONE_NAME" "$test_file" "$test_file_two"
+  run list_objects_check_params_get_token "$BUCKET_ONE_NAME" "$test_file" "$test_file_two" "TRUE"
   assert_success
   continuation_token=$output
 
   # interestingly, AWS appears to accept continuation tokens that are a few characters off, so have to remove three chars
   run list_objects_check_continuation_error "$BUCKET_ONE_NAME" "${continuation_token:0:${#continuation_token}-3}"
+  assert_success
+}
+
+@test "REST - list objects v1 - initial marker empty" {
+  run setup_bucket "s3api" "$BUCKET_ONE_NAME"
+  assert_success
+
+  test_file="test_file"
+  test_file_two="test_file_2"
+  run create_test_files "$test_file" "$test_file_two"
+  assert_success
+
+  run put_object "s3api" "$TEST_FILE_FOLDER/$test_file" "$BUCKET_ONE_NAME" "$test_file"
+  assert_success
+
+  run put_object "s3api" "$TEST_FILE_FOLDER/$test_file_two" "$BUCKET_ONE_NAME" "$test_file_two"
+  assert_success
+
+  run list_objects_v1_check_initial_marker_empty "$BUCKET_ONE_NAME"
   assert_success
 }
