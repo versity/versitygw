b5b418e4b398460de40d2e80bc5bf8d3141500ec
niksis02 <sisnikoyan@gmail.com>
2025-01-17 03:19:34 +0400

    fix: Adds the minimum allowed size(5 Mib) check for mp parts sizes in CompleteMultipartUpload

--- tests/integration/tests.go
+++ tests/integration/tests.go
@@ -7192,6 +7192,49 @@ func CompleteMultipartUpload_invalid_ETag(s *S3Conf) error {
 	})
 }
 
+func CompleteMultipartUpload_small_upload_size(s *S3Conf) error {
+	testName := "CompleteMultipartUpload_small_upload_size"
+	return actionHandler(s, testName, func(s3client *s3.Client, bucket string) error {
+		obj := "my-obj"
+
+		mp, err := createMp(s3client, bucket, obj)
+		if err != nil {
+			return err
+		}
+
+		// The uploaded parts size is 256 < 5 Mib (the minimum allowed size)
+		parts, _, err := uploadParts(s3client, 1024, 4, bucket, obj, *mp.UploadId)
+		if err != nil {
+			return err
+		}
+
+		cParts := []types.CompletedPart{}
+
+		for _, el := range parts {
+			cParts = append(cParts, types.CompletedPart{
+				PartNumber: el.PartNumber,
+				ETag:       el.ETag,
+			})
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), shortTimeout)
+		_, err = s3client.CompleteMultipartUpload(ctx, &s3.CompleteMultipartUploadInput{
+			Bucket:   &bucket,
+			Key:      &obj,
+			UploadId: mp.UploadId,
+			MultipartUpload: &types.CompletedMultipartUpload{
+				Parts: cParts,
+			},
+		})
+		cancel()
+		if err := checkApiErr(err, s3err.GetAPIError(s3err.ErrEntityTooSmall)); err != nil {
+			return err
+		}
+
+		return nil
+	})
+}
+
 func CompleteMultipartUpload_empty_parts(s *S3Conf) error {
 	testName := "CompleteMultipartUpload_empty_parts"
 	return actionHandler(s, testName, func(s3client *s3.Client, bucket string) error {
@@ -7233,7 +7276,7 @@ func CompleteMultipartUpload_success(s *S3Conf) error {
 			return err
 		}
 
-		objSize := int64(5 * 1024 * 1024)
+		objSize := int64(25 * 1024 * 1024)
 		parts, csum, err := uploadParts(s3client, objSize, 5, bucket, obj, *out.UploadId)
 		if err != nil {
 			return err
@@ -7326,7 +7369,7 @@ func CompleteMultipartUpload_racey_success(s *S3Conf) error {
 		var mu sync.RWMutex
 		uploads := make([]mpinfo, 10)
 		sums := make([]string, 10)
-		objSize := int64(5 * 1024 * 1024)
+		objSize := int64(25 * 1024 * 1024)
 
 		eg := errgroup.Group{}
 		for i := 0; i < 10; i++ {
@@ -12990,7 +13033,7 @@ func Versioning_Multipart_Upload_success(s *S3Conf) error {
 			return err
 		}
 
-		objSize := int64(5 * 1024 * 1024)
+		objSize := int64(25 * 1024 * 1024)
 		parts, _, err := uploadParts(s3client, objSize, 5, bucket, obj, *out.UploadId)
 		if err != nil {
 			return err
@@ -13070,7 +13113,7 @@ func Versioning_Multipart_Upload_overwrite_an_object(s *S3Conf) error {
 			return err
 		}
 
-		objSize := int64(5 * 1024 * 1024)
+		objSize := int64(25 * 1024 * 1024)
 		parts, _, err := uploadParts(s3client, objSize, 5, bucket, obj, *out.UploadId)
 		if err != nil {
 			return err
