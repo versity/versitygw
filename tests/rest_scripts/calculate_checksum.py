381958e75112f91987683f2c026fe70526c4b5c8
Luke McCrone <lrm110484@gmail.com>
2025-04-04 14:20:58 -0300

    test: update calculate_checksum.py

--- tests/rest_scripts/calculate_checksum.py
+++ tests/rest_scripts/calculate_checksum.py
@@ -1,13 +1,21 @@
 import sys
 from awscrt import checksums
 
+def get_call(checksum_type):
+    if checksum_type == "crc32c":
+        return checksums.crc32c
+    elif checksum_type == "crc64nvme":
+        return checksums.crc64nvme
+    sys.stderr.write("unrecognized checksum type " + checksum_type)
+    sys.exit(1)
+
 def main():
     if len(sys.argv) != 3:
-        print('Checksum type, data file required')
+        sys.stderr.write('Checksum type, data file path required')
         sys.exit(1)
-    checksum_type=sys.argv[1]
     with open(sys.argv[2], 'rb') as f:
-      print(checksums.checksum_type(f.read())
+        function = get_call(sys.argv[1])
+        print(function(f.read()))
 
 if __name__ == "__main__":
     main()
