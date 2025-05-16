ff1620819659512c92c30531bea5ab39e88f9446
Luke McCrone <lrm110484@gmail.com>
2025-05-16 17:08:14 -0300

    test: add source

--- tests/rest_scripts/put_object_lock_configuration.sh
+++ tests/rest_scripts/put_object_lock_configuration.sh
@@ -14,6 +14,10 @@
 # specific language governing permissions and limitations
 # under the License.
 
+source ./tests/rest_scripts/rest.sh
+
+# Fields
+
 # shellcheck disable=SC2153
 bucket_name="$BUCKET_NAME"
 # shellcheck disable=SC2153
