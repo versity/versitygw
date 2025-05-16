ea807ae590d8b7191fa982037d54dc1352ec0f3f
Luke McCrone <lrm110484@gmail.com>
2025-05-16 14:02:00 -0300

    test: complete bucket setup conversion to REST

--- tests/util/util_ownership.sh
+++ tests/util/util_ownership.sh
@@ -1,20 +1,10 @@
 #!/usr/bin/env bash
 
 get_and_check_ownership_controls() {
-  if [ $# -ne 2 ]; then
-    log 2 "'get_and_check_ownership_controls' missing bucket name, expected result"
+  if ! check_param_count "get_and_check_ownership_controls" "bucket, expected result" 2 $#; then
     return 1
   fi
-  if ! result=$(COMMAND_LOG="$COMMAND_LOG" BUCKET_NAME="$BUCKET_ONE_NAME" OUTPUT_FILE="$TEST_FILE_FOLDER/ownershipControls.txt" ./tests/rest_scripts/get_bucket_ownership_controls.sh); then
-    log 2 "error getting bucket ownership controls: $result"
-    return 1
-  fi
-  if [ "$result" != "200" ]; then
-    log 2 "GetBucketOwnershipControls returned response code: $result, reply:  $(cat "$TEST_FILE_FOLDER/ownershipControls.txt")"
-    return 1
-  fi
-  log 5 "controls: $(cat "$TEST_FILE_FOLDER/ownershipControls.txt")"
-  if ! rule=$(xmllint --xpath '//*[local-name()="ObjectOwnership"]/text()' "$TEST_FILE_FOLDER/ownershipControls.txt" 2>&1); then
+  if ! rule=$(get_bucket_ownership_controls_rest "$1" 2>&1); then
     log 2 "error getting ownership rule: $rule"
     return 1
   fi
