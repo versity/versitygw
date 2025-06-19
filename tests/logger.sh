23007f41983397dc6808142ebc6d59bab038be71
Luke McCrone <lrm110484@gmail.com>
2025-06-19 15:26:43 -0300

    test: fix

--- tests/logger.sh
+++ tests/logger.sh
@@ -40,17 +40,6 @@ log() {
 }
 
 log_with_stack_ref() {
-  if ! check_log_params "log_with_stack_ref" "level, message, stack reference" 3 $#; then
-    return 1
-  fi
-  if ! log_with_stack_ref "$1" "$2" 2; then
-    echo "error logging with stack ref"
-    return 1
-  fi
-  return 0
-}
-
-log_with_stack_ref() {
   if [[ $# -ne 3 ]]; then
     echo "log_with_stack_ref function requires level, message, stack reference"
     return 1
