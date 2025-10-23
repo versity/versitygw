a0090392f899b810138cf2a3069b4ff054e2f431
Luke McCrone <lrm110484@gmail.com>
2025-10-23 15:04:55 -0300

    test: safety commit

--- tests/rest_scripts/logger/logging.go
+++ tests/rest_scripts/logger/logging.go
@@ -1,11 +1,23 @@
 package logger
 
-import "log"
+import (
+	"log"
+	"os"
+)
 
 var Debug *bool
+var LogFile *string
 
-func PrintDebug(format string, args ...any) {
+func PrintDebug(format string, args ...interface{}) {
 	if *Debug {
+		if *LogFile != "" {
+			logFile, err := os.OpenFile(*LogFile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+			if err != nil {
+				log.Fatalf("Error opening logfile: %v", err)
+			}
+			defer logFile.Close()
+			log.SetOutput(logFile)
+		}
 		log.Printf(format, args...)
 	}
 }
