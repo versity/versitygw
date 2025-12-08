ae1206e4f2b0ac86531716f3a510f99bf690ce78
Luke McCrone <lrm110484@gmail.com>
2025-12-08 18:54:48 -0300

    test: util cleanup

--- tests/drivers/get_bucket_location/get_bucket_location.sh
+++ tests/drivers/get_bucket_location/get_bucket_location.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright 2024 Versity Software
+# Copyright 2025 Versity Software
 # This file is licensed under the Apache License, Version 2.0
 # (the "License"); you may not use this file except in compliance
 # with the License.  You may obtain a copy of the License at
@@ -14,13 +14,18 @@
 # specific language governing permissions and limitations
 # under the License.
 
-setup_two_buckets() {
-  setup_bucket "$BUCKET_ONE_NAME" || local setup_result_one=$?
-  if [[ $setup_result_one -eq 0 ]]; then
+get_check_bucket_location_various() {
+  if [ $# -ne 2 ]; then
+    log 2 "'get_bucket_location' requires client, bucket"
     return 1
   fi
-  setup_bucket "$BUCKET_TWO_NAME" || local setup_result_two=$?
-  if [[ $setup_result_two -eq 0 ]]; then
+  if ! get_bucket_location "$1" "$2"; then
+    log 2 "error getting bucket location"
+    return 1
+  fi
+  # shellcheck disable=SC2154
+  if [[ $bucket_location != "null" ]] && [[ $bucket_location != "us-east-1" ]]; then
+    log 2 "wrong location: '$bucket_location'"
     return 1
   fi
   return 0
