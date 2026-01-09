32c51b72cfc31b679af8feb452928c1a3ad26a18
Luke McCrone <lrm110484@gmail.com>
2026-01-09 19:42:09 -0300

    test: temp

--- tests/drivers/list_object_versions/list_object_versions_rest.sh
+++ tests/drivers/list_object_versions/list_object_versions_rest.sh
@@ -48,3 +48,76 @@ get_non_latest_version() {
   fi
   return 0
 }
+
+check_object_versions_before_deletion() {
+  if ! check_param_count_v2 "data file" 1 $#; then
+    return 1
+  fi
+  if ! get_xml_data "$1" "$1.tmp"; then
+    log 2 "error getting XML data"
+    return 1
+  fi
+  if ! parse_versions_rest "$1.tmp"; then
+    log 2 "error parsing versions"
+    return 1
+  fi
+  if [ "${#version_ids[@]}" -ne 1 ]; then
+    log 2 "expected version ID count of 1, was '${#version_ids[@]}'"
+    return 1
+  fi
+  version_id="${version_ids[0]}"
+  log 5 "version ID: $version_id"
+  return 0
+}
+
+check_object_versions_after_deletion() {
+  if ! check_param_count_v2 "data file" 1 $#; then
+    return 1
+  fi
+  if ! get_xml_data "$1" "$1.tmp"; then
+    log 2 "error getting XML data"
+    return 1
+  fi
+  if ! parse_versions_rest "$1.tmp"; then
+    log 2 "error parsing versions"
+    return 1
+  fi
+  if [ "${#version_ids[@]}" -ne 2 ]; then
+    log 2 "expected version ID count of 2, was '${#version_ids[@]}'"
+    return 1
+  fi
+  if [ "${version_ids[0]}" != "$version_id" ]; then
+    log 2 "expected version ID of '$version_id', was '${version_ids[0]}'"
+    return 1
+  fi
+  if [ "${version_islatests[0]}" != "false" ]; then
+    log 2 "expected 'IsLatest' of version ID to be false, was '${version_islatests[0]}'"
+    return 1
+  fi
+  if [ "${version_islatests[1]}" != "true" ]; then
+    log 2 "expected 'IsLatest' of delete marker to be true, was '${version_islatests[1]}'"
+    return 1
+  fi
+  return 0
+}
+
+list_object_versions_before_and_after_retention_deletion() {
+  if ! check_param_count_v2 "bucket name, file" 2 $#; then
+    return 1
+  fi
+  if ! send_rest_go_command_callback "200" "check_object_versions_before_deletion" \
+      "-method" "GET" "-bucketName" "$1" "-query" "versions="; then
+    log 2 "error checking versions before deletion"
+    return 1
+  fi
+  if ! delete_object_rest "$1" "$2"; then
+    log 2 "error deleting file"
+    return 1
+  fi
+  if ! send_rest_go_command_callback "200" "check_object_versions_after_deletion" \
+      "-method" "GET" "-bucketName" "$1" "-query" "versions="; then
+    log 2 "error checking versions before deletion"
+    return 1
+  fi
+  return 0
+}
